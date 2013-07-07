14c9272491cecfa7fad993a3e8b86443aede853d
tyrel86 <tyrelrichey@gmail.com>
2013-07-07 13:32:30 -0600

    Defautl Avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -11,6 +11,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+	def default_url
+		"/assets/defaults/avatar#{version_name ? '_' + version_name.to_s : ''}.png"
+  end
+
 	process :resize_to_limit => [400, 0]
 
   version :thumb do
