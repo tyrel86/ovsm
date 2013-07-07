2be2ebee4baf02b9b1c4b73ce191ca44e35288b6
tyrel86 <tyrelrichey@gmail.com>
2013-07-07 13:01:19 -0600

    Bug Fixes

--- app/controllers/feed_videos_controller.rb
+++ app/controllers/feed_videos_controller.rb
@@ -1,5 +1,6 @@
 class FeedVideosController < ApplicationController
 	def show
 		@video = FeedVideo.for_feed( Feed.find_by_slug( params[:feed_id] ) ).first
+		@feed = @video.feed
 	end
 end
--- app/models/feed_video.rb
+++ app/models/feed_video.rb
@@ -5,4 +5,8 @@ class FeedVideo < ActiveRecord::Base
 
 	scope :active, where( active: true )
 	scope :for_feed, lambda { |feed| where( feed_id: feed.id ).active }
+
+	def self.url_for( feed, encoding )
+		"https://overseame.s3.amazonaws.com/uploads/#{Rails.env}/feed_video/#{feed.slug}/current.#{encoding}"
+	end
 end
